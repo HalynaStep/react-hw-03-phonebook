21065a6b8de28d1cff86a14ac538a58b5569d4af
HalynaStep <gstep2003@gmail.com>
2022-10-08 16:22:57 +0300

    clone hw-02

---
+++ src/components/ContactList/listItem.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Item = styled.li`
+  display: flex;
+
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Text = styled.p`
+  margin-right: 10px;
+  margin-bottom: 10px;
+  font-size: 16px;
+`;
+
+export const Button = styled.button`
+  height: 20px;
+  font-size: 12px;
+  text-content: center;
+`;
